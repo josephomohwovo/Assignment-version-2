1d1853ab4f97abf3b0de46d3c37f648fb539acb3
josephomohwovo <josephomohwovo@gmail.com>
2023-01-24 21:57:26 +0100

    Assingmen2:

---
+++ SecondAssaignment.js
@@ -0,0 +1,14 @@
+// create a function that returns my name.
+// Takes my firstname and lastname as parameters
+// also age para
+
+// sltn
+// My ezekeil Adetoro I am 23 years old
+function fullName(firstName, lastName){
+    return firstName + " " + lastName;
+  }
+  
+  //calling the function and assigning the return to a variable
+  var fullname = fullName("Brad", "Pitt");
+  
+  console.log(fullname)
